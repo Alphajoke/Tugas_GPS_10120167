742c26476da373d77436f57166deddbc4e5b6f92
Bintang <bintanghakim74@gmail.com>
2023-06-21 13:12:34 +0700

    TugasAKB

--- app/src/androidTest/java/com/bintang/tugas_sensorgps_10120167/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/bintang/tugas_sensorgps_10120167/ExampleInstrumentedTest.java
@@ -21,6 +21,6 @@ public class ExampleInstrumentedTest {
     public void useAppContext() {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
-        assertEquals("com.rizkyrazak.tugas_sensorgps_10119118", appContext.getPackageName());
+        assertEquals("com.bintang.tugas_sensorgps_10120167", appContext.getPackageName());
     }
 }
